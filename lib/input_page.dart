f312d07e1385c4bfe423b72c0e706008b09a970c
TechSavvyNaz <nazishnaseer50@gmail.com>
2024-11-05 01:19:41 +0500

    using container widget for deigning
    Flutter in Urdu | BMI Calculator Design Part 2 Lecture 37 | #flutterinhindi ‪@TechByAbdullah79‬

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,8 +14,56 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI Calculator'),
       ),
-      body: const Center(
-        child: Text('Body Content Here'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius:BorderRadius.circular(10.0) ,
+                  ),
+                )), // Replace null with Container()
+                Expanded(child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius:BorderRadius.circular(10.0) ,
+                  ),
+                )), // Replace null with Container()
+              ],
+            ),
+          ),
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius:BorderRadius.circular(10.0) ,
+            ),
+          )),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius:BorderRadius.circular(10.0) ,
+                  ),
+                )),
+                Expanded(child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius:BorderRadius.circular(10.0) ,
+                  ),
+                )),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
